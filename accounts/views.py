cdfb8e3c8aff9009950a873d4ebd4d99c1b06185
KruzeZab <60575709+KruzeZab@users.noreply.github.com>
2021-02-12 23:35:04 +0545

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
 from django.views.generic import ListView
 from django.contrib import messages, auth
 from .models import Contact
@@ -19,16 +20,16 @@ def login(request):
         if user is not None:
             auth.login(request, user)
             messages.success(request, 'You are now logged in')
-            return redirect('/accounts/'+str(request.user.id))
+            return HttpResponseRedirect('/accounts/'+str(request.user.id))
         else:
             messages.error(request, 'Invalid credentials')
-            return redirect('accounts:login')
+            return HttpResponseRedirect('/accounts/login')
     else:
         return render(request, 'accounts/login.html')
 
 def register(request):
     if request.user.is_authenticated:
-        return redirect('pages:index')
+        return HttpResponseRedirect('/')
     if request.method == 'POST':
         # Get form values
         first_name = request.POST['firstname']
@@ -43,22 +44,22 @@ def register(request):
         # Check username
             if User.objects.filter(username=username).exists():
                 messages.error(request, 'That username is taken')
-                return redirect('accounts:register')
+                return HttpResponseRedirect('/accounts/register')
             else:
                 if User.objects.filter(email=email).exists():
                     messages.error(request, 'That email is being used')
-                    return redirect('accounts:register')
+                    return HttpResponseRedirect('/accounts/register')
                 else:
                     # Looks good
                     user = User.objects.create_user(username=username, password=password,email=email, first_name=first_name, last_name=last_name)
                     # Login after register
                     auth.login(request, user)
                     messages.success(request, 'You are now logged in')
-                    return redirect('listings:index')
+                    return HttpResponseRedirect('/listings')
                     
         else:
             messages.error(request, 'Passwords do not match')
-            return redirect('accounts:register')
+            return HttpResponseRedirect('/accounts/register')
     else:
         return render(request, 'accounts/register.html')
 
